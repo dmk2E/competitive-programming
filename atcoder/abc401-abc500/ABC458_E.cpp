9535005aa0c8091bb9936dbb6c3d38f8acb9e9d5
KouE10e <210100037+dmk2E@users.noreply.github.com>
2026-05-25 12:10:36 +0900

    feat(ABC458): Add a solution for problem E

--- atcoder/abc401-abc500/ABC458_E.cpp
+++ atcoder/abc401-abc500/ABC458_E.cpp
@@ -1,3 +1,11 @@
+/*アルゴリズムと工夫点(Count 123/CPU: 60 ms Memory: 50468 KB  Length: 3398 B)
+グループ数を固定して数え上げる．
+条件を満たす数列とは，「1」と「3」が隣り合わない事である．
+そこで，「1」と「3」のグループ数を全探索し，その数が固定されている場合に，
+1~3 の数の振り分け方を計算すればよい．
+高速計算の為，逆元を利用する．
+最悪計算量は，O(X1 + X2 + X3) < 10 ^ 7 となり高速．
+*/
 #include<iostream>
 #include<vector>
 #include<cassert>
@@ -44,7 +52,7 @@ public:
         for(i = this -> max_num - 1;i >= 0;i--)inv_fact[i] = (inv_fact[i + 1] * (i + 1)) % this -> mod;
     }
 
-    /// @brief N個の中からr個だけ選ぶ組合せの総数を求める
+    /// @brief n個の中からr個だけ選ぶ組合せの総数を求める
     /// @param n 選択肢の数
     /// @param r 選ぶ数
     /// @return nCr を mod で割った余り
@@ -61,16 +69,37 @@ public:
         // r個のボールを，n - 1 本の線で区切る方法の総数を求めると考えられるから，
         // 「n + r - 1 個の選択肢の内，r個だけボールとする場合の数」と言い換えられる
         return this -> comb(/* n = */ n + r - 1, r);
-    }
+    }    
 };
 
+int x_1, x_2, x_3;
+
 int main(){
-    // お試し
-    Combination comb(/* max_num = */ 1000000);
-    while(1){
-        int n, r;
-        scanf("%d%d", &n, &r);
-        printf("%lld\n", comb.comb(/* n = */ n, /* r = */ r));
+    int i, j;
+    scanf("%d%d%d", &x_1, &x_2, &x_3);
+
+    Combination comb(/* max_num = */ x_1 + x_2 + x_3);
+    ll ans = 0;
+    rep(i, 2){
+        for(int sum_group = 2;;sum_group++){
+            int group_1 = sum_group / 2, group_3 = sum_group - group_1;
+            if(group_1 > x_1 || group_3 > x_3)break;
+            if(x_2 < sum_group - 1)break;
+
+            int rest_1 = x_1 - group_1;
+            ll now = comb.countDistributions(/* n = */ group_1, /* r = */ rest_1);
+            
+            int rest_3 = x_3 - group_3;
+            now = (now * comb.countDistributions(/* n = */ group_3, /* r = */ rest_3)) % MOD;
+            
+            int rest_2 = x_2 - (sum_group - 1);
+            now = (now * comb.countDistributions(/* n = */ x_1 + x_3 + 1, /* r = */ rest_2)) % MOD;
+
+            ans = (ans + now) % MOD;
+        }
+        swap(x_1, x_3);
     }
+
+    printf("%lld\n", ans);
     return 0;
 }
